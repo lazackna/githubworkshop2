5ee3f851e31f23c322d816138b220e5b411d4129
jasper baijens <jasperbayens@outlook.com>
2021-02-10 16:22:54 +0100

    Added method multiply

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,16 @@ public class Main {
 	    double r = sum(a,b);
 
         System.out.println(a + " + " + b + " = " + r);
+
+        double r2 = multiply(a, b);
+        System.out.println(a + " * " + b + " = " + r2);
     }
 
     public static double sum (double a, double b) {
         return  a + b;
+    }
+
+    public static double multiply (double a, double b) {
+        return  a * b;
     }
 }
