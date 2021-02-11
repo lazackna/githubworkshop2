d9bafa85a0b5625548cadaea9d236a18ebb75d01
jasper baijens <jasperbayens@outlook.com>
2021-02-11 16:24:19 +0100

    changed sum

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
     }
 
     public static double sum (double a, double b) {
-        return  a + b;
+        return  a * b;
     }
 
     public static double subtract (double a, double b) {
